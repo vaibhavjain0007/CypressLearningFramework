63ab41557096aa974b62c9cd6666c9b8906f01f7
Vaibhav Jain <vaibhav25011998@gmail.com>
2024-11-28 11:39:50 +0530

    modified Need2Knowmjs

--- cypress/e2e/Need2Know.cy.js
+++ cypress/e2e/Need2Know.cy.js
@@ -1,6 +1,8 @@
-cy.get('.loading-spinner', { timeout: 10000 }).should('not.exist'); // { timeout : timeInMs 
+cy.get('.loading-spinner', { timeout: 10000 }).should('not.exist'); // timeout : timeInMs 
 cy.wait(2000);  // Hardcoded wait for 2 seconds (not recommended)
 cy.get('.loading-spinner').should('not.exist'); // default wait (4 seconds)
 // In Cypress, polling is the mechanism used to repeatedly check the state of an element or 
   // condition during a command's execution, such as waiting for an element to appear, disappear, or change.
 // Polling interval in cypress ---> 50ms
+// npx cypress run --force
+  //The --force flag will ignore any .only or .skip applied to tests and will run all tests in your Cypress suite.
